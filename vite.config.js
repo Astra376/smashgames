e3f9426f3ffc6048323e5bb26a098a7bf7705329
Astra376 <connor.andrews81@outlook.com>
2025-12-07 11:42:18 +1030

    fucking fags

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,19 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  // 1. Translated 'publicPath' to Vite's 'base' property
+  base: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
+
   plugins: [vue()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       'ace-builds': path.resolve(__dirname, 'node_modules/ace-builds')
     }
   },
+
   optimizeDeps: {
     include: ['ace-builds']
   }
-})
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
 })
